f564a31ff1059c29f43dca0ad9425c2f8ffffae3
Steven Hafen <Steven.Hafen@equilife.com>
2020-03-21 14:21:20 -0600

    fixed syntax error

--- app/core/admin.py
+++ app/core/admin.py
@@ -25,7 +25,7 @@ class UserAdmin(BaseUserAdmin):
     )
     add_fieldsets = (
         (None, {
-            'classes': ('wide',)
+            'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
         }),
     )
